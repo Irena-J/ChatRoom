45e8c6d1acd6cf75a62d54384645e14b6a364c75
Jokseia <2426733691@qq.com>
2020-08-29 23:17:02 +0800

    channel

--- ChatRoom-sys/src/main/java/api/ChannelServlet.java
+++ ChatRoom-sys/src/main/java/api/ChannelServlet.java
@@ -2,6 +2,7 @@ package api;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
 import model.Channel;
 import model.ChannelDao;
 import model.User;
@@ -20,16 +21,85 @@ import java.util.List;
 public class ChannelServlet extends HttpServlet {
     private Gson gson = new GsonBuilder().create();
 
+    static class Request {
+        public String channelName;
+        public int channelId;
+    }
+
+    static class Response {
+        public int ok;
+        public String reason;
+    }
+
     // 新增频道
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // TODO 同学们课后自己试着实现下
+        req.setCharacterEncoding("utf-8");
+        Response response = new Response();
+        // 1. 读取 body 中的信息（json格式的字符串）
+
+        try {
+            // 1. 验证登陆状态. 如果未登陆则不能查看.
+            HttpSession httpSession = req.getSession(false);
+            if (httpSession == null) {
+                throw new ChatroomException("您尚未登陆");
+            }
+            String body = Util.readBody(req);
+            // 2. 把 json 数据转成 Java 中的对象.
+            Request request = gson.fromJson(body,Request.class);
+            System.out.println("request:"+request);
+            // 3. 频道可以任意添加的，因为 id 是不同的
+
+            ChannelDao channelDao = new ChannelDao();
+
+            Channel channel = new Channel();
+            channel.setChannelName(request.channelName);
+            channelDao.add(channel);
+
+            response.ok = 1;
+            response.reason = "";
+        } catch (ChatroomException | JsonSyntaxException e) {
+            e.printStackTrace();
+            response.ok = 0;
+            response.reason = e.getMessage();
+        } finally {
+            resp.setContentType("application/json;charset=utf-8");
+            String jsonString = gson.toJson(response);
+            resp.getWriter().write(jsonString);
+        }
     }
 
     // 删除频道
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // TODO 同学们课后自己试着实现下
+        req.setCharacterEncoding("UTF-8");
+        Response response = new Response();
+        // 1. 读取 body 中的信息
+        try {
+            // 1. 验证登陆状态. 如果未登陆则不能查看.
+            HttpSession httpSession = req.getSession(false);
+            if (httpSession == null) {
+                throw new ChatroomException("您尚未登陆");
+            }
+            String body = Util.readBody(req);
+            // 2. 把 json 数据转成 Java 中的对象
+            Request request = gson.fromJson(body,Request.class);
+            System.out.println("request:"+request);
+            ChannelDao channelDao = new ChannelDao();
+            channelDao.delete(request.channelId);
+
+            response.ok = 1;
+            response.reason = "";
+        } catch (ChatroomException | JsonSyntaxException e) {
+            e.printStackTrace();
+            response.ok = 0;
+            response.reason = e.getMessage();
+        } finally {
+            resp.setContentType("application/json; charset=utf-8");
+            String jsonString = gson.toJson(response);
+            resp.getWriter().write(jsonString);
+        }
+
     }
 
     // 获取频道列表
